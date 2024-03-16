5fc5fbfe45263639880d71240e28d86d79676f3d
MoizLokh <mlokhandwala360@gmail.com>
2024-03-16 10:40:44 -0700

    Fixed unit test files

--- projects/base-library/project/Tests/utest.c
+++ projects/base-library/project/Tests/utest.c
@@ -7,7 +7,7 @@
 #include "utest.h"
 #include "uconfig.h"
 
-/*
+
 //-- Add tests to runner and custom test macros/definitions
 #define NUM_ITERS 10
 
@@ -38,4 +38,4 @@ testresult iteration_test(void) {
 void run_tests(void) {
 	test_main(test_runner, sizeof(test_runner) / sizeof(t_test));
 }
-*/
+
