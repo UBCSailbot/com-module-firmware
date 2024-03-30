8bbc1e3df44293e0e4747df220eb65ca446b57df
MoizLokh <mlokhandwala360@gmail.com>
2024-03-30 15:26:40 -0700

    Initial error baselib interface

---
+++ projects/base-library/project/Core/Inc/error.h
@@ -0,0 +1,46 @@
+/*
+ * error.h
+ *
+ *  Created on: Mar 30, 2024
+ *      Author: mlokh
+ */
+
+#ifndef INC_ERROR_H_
+#define INC_ERROR_H_
+
+#define ERROR_LOG_SIZE 10
+
+typedef enum
+{
+	ERR_LOW,
+	ERR_MID,
+	ERR_HIGH
+}errlevel;
+
+typedef enum
+{
+	SLOW_BLINK,
+	FAST_BLINK,
+}ledpattern;
+
+typedef enum
+{
+	OK,
+	ERROR
+} errcode;
+
+
+errcode errlog[ERROR_LOG_SIZE];
+uint8_t index;
+
+//-- Internal functions
+static void reportError(errcode err_code, errlevel err_level); // Sending all accumulated error codes through UART and/or through CAN
+static void logError(errcode err_code); // Add error to error logs. Can be stored in memory
+static void ledIndicationError(ledpattern pattern);
+
+//-- Public Functions
+#define REPORT_ERR(err_code, err_level) (reportError(err_code, err_level))
+errcode getLatestError(void);
+void clearAllErrors(void);
+
+#endif /* INC_ERROR_H_ */
