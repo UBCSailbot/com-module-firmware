411537a91519c7f2777cf05eff5c6f6c1169439a
alisha1697 <143849351+alisha1697@users.noreply.github.com>
2024-12-16 13:47:47 -0800

    I2C_ErrorResetCycle in error.c (#21)
    * I2C_ErrorResetCycle in error.c
    
    * error fix

--- projects/base-library/project/Core/Inc/error.h
+++ projects/base-library/project/Core/Inc/error.h
@@ -6,6 +6,7 @@
  *  Created on: Mar 30, 2024
  *  Author: Moiz
  */
+#include <cstdint>
 
 #ifndef INC_ERROR_H_
 #define INC_ERROR_H_
@@ -41,6 +42,7 @@ uint8_t index;
 static void reportError(errcode err_code, errlevel err_level); // Sending all accumulated error codes through UART and/or through CAN
 static void logError(errcode err_code); // Add error to error logs. Can be stored in memory
 static void ledIndicationError(ledpattern pattern);
+static void I2C_ErrorResetCycle(I2C_HandleTypeDef handle, uint16_t device_address, uint8_t register_address, int read_regs);
 
 //-- Public Functions
 #define REPORT_ERR(err_code, err_level) (reportError(err_code, err_level))
