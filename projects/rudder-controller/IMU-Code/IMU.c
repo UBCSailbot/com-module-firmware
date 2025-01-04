448c0e417b31350d3ebdf00dde683bcb34580478
Michael Greenough <michaellg2004@gmail.com>
2025-01-03 21:04:32 -0800

    Fixed timer reset

--- projects/rudder-controller/IMU-Code/IMU.c
+++ projects/rudder-controller/IMU-Code/IMU.c
@@ -54,6 +54,7 @@ void IMU__init(IMU* self, I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timC
 	//Adjust the timer so that we get interrupts on the specified frequency
 	__HAL_TIM_SET_PRESCALER(timChannel, PRESCALAR);
 	__HAL_TIM_SET_AUTORELOAD(timChannel, APB1_FREQUENCY/(PRESCALAR+1)*timeBetweenSamples);
+	__HAL_TIM_SET_COUNTER(timChannel, 0);
 
 	//Transmit the instruction to get into IMU mode
 	HAL_I2C_Master_Transmit(self->I2cHandle, BNO055_ADDR, (uint8_t *) imuModeData, 2, HAL_MAX_DELAY);
