b81b58fdc1da9929781788745c4b637e8938df0b
alisha1697 <143849351+alisha1697@users.noreply.github.com>
2025-03-08 19:41:46 -0800

    can.h first commit
    Should work? unless I overlooked something or tested it wrong
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>

--- projects/base-library/project/Core/Inc/can.h
+++ projects/base-library/project/Core/Inc/can.h
@@ -1,14 +1,24 @@
-/*
- *  can.h
- *
- *  Description: Provides declarations for variables and function prototypes related to CAN.
- *
- *  Created on: April 4, 2024
- *  Author: Peter, Jordan, Katherine
- */
+#ifndef SRC_CAN_H_
+#define SRC_CAN_H_
 
-/* Includes ------------------------------------------------------------------*/
+#include "stm32u5xx_hal.h"
+#include "stm32u5xx_hal_fdcan.h"
+#include <stdint.h>
+#include <stdlib.h>
 
-/* Variables ------------------------------------------------------------------*/
+extern FDCAN_HandleTypeDef hfdcan1;
 
-/* Functions ------------------------------------------------------------------*/
+void CAN_SetRxBufferSize(uint16_t RxData1_Length, uint16_t RxData2_Length);
+void CAN_Init(void);
+
+HAL_StatusTypeDef CAN_Transmit(uint32_t Identifier, uint32_t IdType, uint32_t DataLength, uint8_t* DataBuffer);
+
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs);
+void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo1ITs);
+
+extern uint8_t* RxData1;
+extern uint8_t* RxData2;
+extern uint16_t RxData1_Length;
+extern uint16_t RxData2_Length;
+
+#endif /* SRC_CAN_H_ */
