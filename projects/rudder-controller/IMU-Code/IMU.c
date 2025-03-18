687849d5bfb0a91925be62e04ef2056c41add5aa
hml04 <160178026+hml04@users.noreply.github.com>
2025-03-17 19:53:18 -0700

    Add files via upload
    Signed-off-by: hml04 <160178026+hml04@users.noreply.github.com>

--- projects/rudder-controller/IMU-Code/IMU.c
+++ projects/rudder-controller/IMU-Code/IMU.c
@@ -17,7 +17,7 @@
 #define BNO055_ADDR 0x28 << 1
 
 //The maximum number of sequential register bytes that can be read:
-#define MAX_INPUT_BUFFER 2
+#define MAX_INPUT_BUFFER 18
 
 //The timer prescalar value
 #define PRESCALAR 1599
@@ -25,15 +25,21 @@
 //The APB1 frequency in kHz
 #define APB1_FREQUENCY 160000
 
-//The constant to convert from BNO055 heading output to our standard
-#define HEADING_CONSTANT 62.5
+//The constants to convert from BNO055 euler outputs to our standard degrees
+#define EULER_CONSTANT 62.5
+
+//The address used to check the calibration status of the system, gyroscope, accelerometer and magnetometer consecutively
+#define BNO055_CalibStat 0x35
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------- CONSTANTS -----------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//The register address of the heading data for the BNO055
+//The register address of the heading data for the BNO055 (following 6 registers contain data for heading, roll and pitch consecutively)
 const uint8_t headingRegisterAddress = 0x1A;
+
+//The register address of the offset data for the BNO055 accelerometer X-axis LSB (following 18 registers contain data for accelerometer, magnetometer and gyroscope consecutively)
+const uint8_t accX_LSB_RegisterAddress = 0x55;
 
 //The register and value that needs to be sent for the IMU to leave CONFIG mode and enter IMU mode
 const uint8_t imuModeData[] = { 0x3D, 0x08 };
@@ -44,12 +50,24 @@ const uint8_t imuModeData[] = { 0x3D, 0x08 };
 
 
 //Initializes the IMU object
-void IMU__init(IMU* self, I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, uint8_t timeBetweenSamples, volatile uint32_t* headingOutput) {
+void IMU__init(IMU* self, I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, uint8_t timeBetweenSamples, volatile uint32_t* headingOutput, volatile uint32_t* rollOutput, volatile uint32_t* pitchOutput, volatile uint32_t* accX_Offset, volatile uint32_t* accY_Offset, volatile uint32_t* accZ_Offset, volatile uint32_t* magX_Offset, volatile uint32_t* magY_Offset, volatile uint32_t* magZ_Offset, volatile uint32_t* gyrX_Offset, volatile uint32_t* gyrY_Offset, volatile uint32_t* gyrZ_Offset) {
 
 	//Copy the required data to the IMU object
 	self->I2cHandle = i2cChannel;
 	self->timHandle = timChannel;
 	self->headingOutput = headingOutput;
+	self->rollOutput = rollOutput;
+	self->pitchOutput = pitchOutput;
+	self->data_flag = 0;
+	self->accX_Offset = accX_Offset;
+	self->accY_Offset = accY_Offset;
+	self->accZ_Offset = accZ_Offset;
+	self->magX_Offset = magX_Offset;
+	self->magY_Offset = magY_Offset;
+	self->magZ_Offset = magZ_Offset;
+	self->gyrX_Offset = gyrX_Offset;
+	self->gyrY_Offset = gyrY_Offset;
+	self->gyrZ_Offset = gyrZ_Offset;
 
 	//Adjust the timer so that we get interrupts on the specified frequency
 	__HAL_TIM_SET_PRESCALER(timChannel, PRESCALAR);
@@ -60,18 +78,74 @@ void IMU__init(IMU* self, I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timC
 	HAL_I2C_Master_Transmit(self->I2cHandle, BNO055_ADDR, (uint8_t *) imuModeData, 2, HAL_MAX_DELAY);
 	HAL_Delay(10);
 
+	//IMU_getOffset(IMU* self);
+
 	//Can someone figure out why this shit hard faults when I put this here instead of in the main code
 	//HAL_TIM_Base_Start_IT(timChannel);
 	//HAL_Delay(timeBetweenSamples);
 }
 
-IMU* IMU__create(I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, uint8_t timeBetweenSamples, volatile uint32_t* headingOutput) {
-	//Allocate memory for the IMU object and intialize it
+IMU* IMU__create(I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, uint8_t timeBetweenSamples, volatile uint32_t* headingOutput,  volatile uint32_t* rollOutput, volatile uint32_t* pitchOutput, volatile uint32_t* accX_Offset, volatile uint32_t* accY_Offset, volatile uint32_t* accZ_Offset, volatile uint32_t* magX_Offset, volatile uint32_t* magY_Offset, volatile uint32_t* magZ_Offset, volatile uint32_t* gyrX_Offset, volatile uint32_t* gyrY_Offset, volatile uint32_t* gyrZ_Offset) {
+	//Allocate memory for the IMU object and initialize it
 	IMU* result = (IMU*)malloc(sizeof(IMU));
 	result->inputBuffer = (uint8_t *) malloc(MAX_INPUT_BUFFER);
-	IMU__init(result, i2cChannel, timChannel, timeBetweenSamples, headingOutput);
+	result->calibStat = (uint8_t *) malloc(1);
+	result->calibObject = (CALIBSTAT*) malloc(sizeof(CALIBSTAT));
+	IMU__init(result, i2cChannel, timChannel, timeBetweenSamples, headingOutput, rollOutput, pitchOutput, accX_Offset, accY_Offset, accZ_Offset, magX_Offset, magY_Offset, magZ_Offset, gyrX_Offset, gyrY_Offset, gyrZ_Offset);
 	return result;
 }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------- Calibration MANAGEMENT ----------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+CALIBSTAT* BNO055_ReadCalibStat(IMU* self){
+	//make this have an if statement for HAL status
+//	if (self->calibStat == NULL) {
+//	    printf("Error: calibStat is NULL, allocating memory...\r\n");
+//	    self->calibStat = (uint8_t*) malloc(1);  // Allocate 1 byte
+//	    if (self->calibStat == NULL) {
+//	        printf("Error: Memory allocation failed!\r\n");
+//	        return NULL;
+//	    }
+//	}
+
+	if (HAL_I2C_Mem_Read(self->I2cHandle, BNO055_ADDR, BNO055_CalibStat, 1, self->calibStat, 1, HAL_MAX_DELAY) != HAL_OK){
+//		printf("Error: failed to read calibration status \r\n");
+	}
+	else {
+		//CALIBSTAT* calibObject;
+		//returnVal = (CALIBSTAT*) malloc(sizeof(CALIBSTAT));
+
+		uint8_t calibVal = *(self->calibStat);
+		self->calibObject->sysStat = (calibVal >> 6) & 0x03;
+		self->calibObject->gyrStat = (calibVal >> 4) & 0x03;
+		self->calibObject->accStat = (calibVal >> 2) & 0x03;
+		self->calibObject->magStat = (calibVal) & 0x03;
+
+		/*self->calibObject->sysStat = ((uint8_t) self->calibStat >> 6) & 0x03;
+		self->calibObject->gyrStat = ((uint8_t) self->calibStat >> 4) & 0x03;
+		self->calibObject->accStat = ((uint8_t) self->calibStat >> 2) & 0x03;
+		self->calibObject->magStat = ((uint8_t) self->calibStat) & 0x03;
+		 */
+
+		/*self->calibObject->sysStat = self->calibStat[0];
+		self->calibObject->sysStat = ((self->calibObject->sysStat << 8) | self->calibStat[1]);
+		self->calibObject->gyrStat = self->calibStat[2];
+		self->calibObject->gyrStat = ((self->calibObject->gyrStat << 6) | self->calibStat[3]);
+		self->calibObject->accStat = self->calibStat[4];
+		self->calibObject->accStat = ((self->calibObject->accStat << 4) | self->calibStat[5]);
+		self->calibObject->magStat = self->calibStat[6];
+		self->calibObject->magStat = ((self->calibObject->magStat << 2) | self->calibStat[7]);
+		 */
+
+		//self->calibObject->gyrStat = (calibStat >> 4) & 0x03;
+		//self->calibObject->accStat = (calibStat >> 2) & 0x03;
+		//self->calibObject->magStat = (calibStat) & 0x03;
+		return self->calibObject;
+	}
+}
+
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------- DATA PARSING FUNCTIONS ---------------------------------------------------------------------------
@@ -79,18 +153,61 @@ IMU* IMU__create(I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, u
 
 void IMU__handleTxDMA(IMU* self, I2C_HandleTypeDef *I2cHandle){
 	if(self->I2cHandle->Instance == I2cHandle->Instance){
-		HAL_I2C_Master_Receive_DMA(I2cHandle, BNO055_ADDR, self->inputBuffer, 2);
+		if(self->data_flag == 1){
+			if(HAL_I2C_Master_Receive_DMA(I2cHandle, BNO055_ADDR, self->inputBuffer, 6) != HAL_OK){
+				printf("Error: failed to receive euler data \r\n");
+			}
+		}
+		else if (self->data_flag == 2){
+			if (HAL_I2C_Master_Receive_DMA(I2cHandle, BNO055_ADDR, self->inputBuffer, 18) != HAL_OK){
+				printf("Error: failed to receive offset data \r\n");
+			}
+		}
 	}
 }
 
 void IMU__handleRxDMA(IMU* self, I2C_HandleTypeDef *I2cHandle){
 	if(self->I2cHandle->Instance == I2cHandle->Instance){
-		self->headingOutput[0] = (self->inputBuffer[1] << 8 | self->inputBuffer[0]) * HEADING_CONSTANT;
+		if(self->data_flag == 1){
+			self->headingOutput[0] = (self->inputBuffer[1] << 8 | self->inputBuffer[0]) * EULER_CONSTANT;
+			self->rollOutput[0] = (self->inputBuffer[2] << 8 | self->inputBuffer[3]);
+			self->pitchOutput[0] = (self->inputBuffer[4] << 8 | self->inputBuffer[5]);
+		}
+		else if(self->data_flag == 2){
+			self->accX_Offset[0] = (self->inputBuffer[1] << 8 | self->inputBuffer[0]) * EULER_CONSTANT;
+			self->accY_Offset[0] = (self->inputBuffer[2] << 8 | self->inputBuffer[3]) * EULER_CONSTANT;
+			self->accZ_Offset[0] = (self->inputBuffer[4] << 8 | self->inputBuffer[5]) * EULER_CONSTANT;
+			self->magX_Offset[0] = (self->inputBuffer[6] << 8 | self->inputBuffer[7]) * EULER_CONSTANT;
+			self->magY_Offset[0] = (self->inputBuffer[8] << 8 | self->inputBuffer[9]) * EULER_CONSTANT;
+			self->magZ_Offset[0] = (self->inputBuffer[10] << 8 | self->inputBuffer[11]) * EULER_CONSTANT;
+			self->gyrX_Offset[0] = (self->inputBuffer[12] << 8 | self->inputBuffer[13]) * EULER_CONSTANT;
+			self->gyrY_Offset[0] = (self->inputBuffer[14] << 8 | self->inputBuffer[15]) * EULER_CONSTANT;
+			self->gyrZ_Offset[0] = (self->inputBuffer[16] << 8 | self->inputBuffer[17]) * EULER_CONSTANT;
+		}
+		self->data_flag = 0;
 	}
 }
 
 void IMU__updateBuffer(IMU* self, TIM_HandleTypeDef* timChannel){
 	if(self->timHandle->Instance == timChannel->Instance){
-		HAL_I2C_Master_Transmit_DMA(self->I2cHandle, BNO055_ADDR, (uint8_t *) &headingRegisterAddress, 1);
+		if(self->data_flag == 0){
+			if(HAL_I2C_Master_Transmit_DMA(self->I2cHandle, BNO055_ADDR, (uint8_t *) &headingRegisterAddress, 1) != HAL_OK){
+				printf("Error: failed to transmit signal for euler data \r\n");
+			}
+			else{
+				self->data_flag = 1;
+			}
+		}
+	}
+}
+
+void IMU_getOffset(IMU* self){
+	if(self->data_flag == 0){
+		if(HAL_I2C_Master_Transmit_DMA(self->I2cHandle, BNO055_ADDR, (uint8_t *) &accX_LSB_RegisterAddress, 1) != HAL_OK){
+			printf("Error: failed to transmit signal for offset data \r\n");
+		}
+		else{
+			self->data_flag = 2;
+		}
 	}
 }
--- projects/rudder-controller/IMU-Code/IMU.h
+++ projects/rudder-controller/IMU-Code/IMU.h
@@ -1,5 +1,5 @@
 /*
- * This library prrovides an interface to communicate between the microcontroller and a BNO055 IMU.
+ * This library provides an interface to communicate between the microcontroller and a BNO055 IMU.
  * It currently has the following functionality:
  *		-Initialize the IMU for use
  *		-Read the heading
@@ -8,7 +8,7 @@
  * 		"IMU__handleTxDMA()" must be called inside "HAL_I2C_MasterTxCpltCallback()"
  * 		"IMU__handleRxDMA()" must be called inside "HAL_I2C_MasterRxCpltCallback()"
  * 		"IMU__updateBuffer()" must be called inside "HAL_TIM_PeriodElapsedCallback()"
- * 		"HAL_TIM_Base_Start_IT()" must be called imediatly after creating the IMU object with the
+ * 		"HAL_TIM_Base_Start_IT()" must be called immediately after creating the IMU object with the
  * 		timer channel handle pointer used in the creation of the IMU object as a parameter.
  * 		More information on the intended use can be found in the associated readme file.
  *
@@ -32,13 +32,39 @@
 //--------------------------------------------------------------------------- STRUCTURES ---------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//The CALIBSTAT data type
+typedef struct {
+	volatile uint8_t sysStat;
+	volatile uint8_t gyrStat;
+	volatile uint8_t accStat;
+	volatile uint8_t magStat;
+} CALIBSTAT;
+
 //The IMU data type
 typedef struct {
 	I2C_HandleTypeDef* I2cHandle;
 	TIM_HandleTypeDef* timHandle;
+	//allocated memory to store euler angle data output
 	volatile uint32_t * headingOutput;
+	volatile uint32_t * rollOutput;
+	volatile uint32_t * pitchOutput;
 	uint8_t * inputBuffer;
+	uint8_t * calibStat;
+	CALIBSTAT* calibObject;
+	//flag determining whether reading euler data or offset data
+	uint8_t data_flag;
+	//allocated memory to store offset data output
+	volatile uint32_t * accX_Offset;
+	volatile uint32_t * accY_Offset;
+	volatile uint32_t * accZ_Offset;
+	volatile uint32_t * magX_Offset;
+	volatile uint32_t * magY_Offset;
+	volatile uint32_t * magZ_Offset;
+	volatile uint32_t * gyrX_Offset;
+	volatile uint32_t * gyrY_Offset;
+	volatile uint32_t * gyrZ_Offset;
 } IMU;
+
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------- OBJECT MANAGEMENT ---------------------------------------------------------------------------
@@ -52,10 +78,27 @@ typedef struct {
  * 			for the periodic calling for data from the sensor.
  * @param timeBetweenSamples The time in ms between sample collection from the sensor.
  * @param headingOutput Points to a location in memory where the heading is automatically updated. It is in the format
- * 			of heading * 1000 in degrees where 0 is north and increasing in the CW direciton. It will be modified
+ * 			of heading * 1000 in degrees where 0 is north and increasing in the CW direction. It will be modified
  * 			whenever new sensor data is received at any point in time.
  */
-IMU* IMU__create(I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, uint8_t timeBetweenSamples, volatile uint32_t* headingOutput);
+IMU* IMU__create(I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, uint8_t timeBetweenSamples, volatile uint32_t* headingOutput, volatile uint32_t* rollOutput, volatile uint32_t* pitchOutput, volatile uint32_t* accX_Offset, volatile uint32_t* accY_Offset, volatile uint32_t* accZ_Offset, volatile uint32_t* magX_Offset, volatile uint32_t* magY_Offset, volatile uint32_t* magZ_Offset, volatile uint32_t* gyrX_Offset, volatile uint32_t* gyrY_Offset, volatile uint32_t* gyrZ_Offset);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------- IMU CALIBRATION -----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* This function should be called when reading the calibration status for the system and three internal sensors
+ *  @param reg is the calibration register 0x35
+ */
+CALIBSTAT* BNO055_ReadCalibStat(IMU* self);
+
+
+/* This function should be called to manually write to the offset registers
+ *
+ */
+//void BNO055_CorrectOffsetRegister(IMU* self);
+
+
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------- IMU METHODS -------------------------------------------------------------------------------
@@ -63,7 +106,7 @@ IMU* IMU__create(I2C_HandleTypeDef* i2cChannel, TIM_HandleTypeDef* timChannel, u
 
 /*
  * This function should be called when there is a I2C DMA transmit callback associated with this IMU sensor.
- * It will check that the I2C handle of the iterrupt matches that of the IMU and then respond accordingly.
+ * It will check that the I2C handle of the interrupt matches that of the IMU and then respond accordingly.
  *
  * @param self The IMU object that transmitted
  * @param I2cHandle is the I2C handle of the interrupt
@@ -72,7 +115,7 @@ void IMU__handleTxDMA(IMU* self, I2C_HandleTypeDef *I2cHandle);
 
 /*
  * This function should be called when there is a I2C DMA receive callback associated with this IMU sensor.
- * It will check that the I2C handle of the iterrupt matches that of the IMU and then respond accordingly.
+ * It will check that the I2C handle of the interrupt matches that of the IMU and then respond accordingly.
  * It will update the output registers of the iMU
  *
  * @param self The IMU object being read
@@ -83,12 +126,20 @@ void IMU__handleRxDMA(IMU* self, I2C_HandleTypeDef *I2cHandle);
 
 /*
  * This function should be called when there is an interrupt from the timer channel input in IMU_create.
- * It will check that the timer channel asscoiated with self is the same as timChannel and if it is
+ * It will check that the timer channel associated with self is the same as timChannel and if it is
  * it will trigger the STM32 to get a measurement of the sensor data.
  *
  * @param self The IMU object that should be updated
  * @param timChannel The timer channel of the interrupt
  */
 void IMU__updateBuffer(IMU* self, TIM_HandleTypeDef* timChannel);
+
+/*
+ * This function should be called when there is an interrupt pushed manually to request for offset data
+ * It will read and save the offset values for recalibration after power off
+ *
+ * @param self The IMU object that should be updated
+ */
+void IMU_getOffset(IMU* self);
 
 #endif /* INC_IMU_H_ */
