e2f8fa5a2a1eb294857a82471c3c0f17f91acad4
faaiqm7 <148652388+faaiqm7@users.noreply.github.com>
2025-03-21 09:00:28 -0700

    Update wind_sensor.h
    Signed-off-by: faaiqm7 <148652388+faaiqm7@users.noreply.github.com>

--- projects/WIND_SENSOR/wind_sensor.h
+++ projects/WIND_SENSOR/wind_sensor.h
@@ -11,18 +11,18 @@
 #define WIND_SENSOR_H
 
 #include "stm32u5xx_hal.h"  // Include HAL library for STM32U5 series
-#include <stdint.h>
 
 //WIND SENTENCE BUFFER SIZE
 #define WIND_BUFFER_SIZE 32
 
 //Global Variables for processing wind sentence data
-extern uint8_t rx_buffer[1] = {0};
-extern uint8_t wind_sentence[WIND_BUFFER_SIZE]; //max sentence identifier length is 10 "PLCJEA870" + \0
+extern uint16_t rx_buffer[1] = {0};
+extern uint16_t wind_sentence[WIND_BUFFER_SIZE]; //max sentence identifier length is 10 "PLCJEA870" + \0
 extern int wind_sentence_index = 0;
-extern uint8_t wind_speed[5]; //in knots
-extern uint8_t wind_direction[5]; //in degrees
-extern uint8_t wind_temperature[5]; //in Degrees Celsius
+extern uint16_t wind_speed[5]; //in knots
+extern uint16_t wind_direction[4]; //in degrees
+extern uint16_t wind_temperature[5]; //in Degrees Celsius
+extern uint16_t wind_data_can_output[32]; //OUTPUT TO CANBUS
 
 
 //Function Handles
@@ -31,6 +31,7 @@ extern uint8_t wind_temperature[5]; //in Degrees Celsius
 void processWindSensorData(uint8_t *input_data);
 //Function for handling DMA on USART2 for the wind sensor
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
-
+//Function for converting ASCII to INT
+int ASCIItoINT(uint8_t input);
 #endif
 
