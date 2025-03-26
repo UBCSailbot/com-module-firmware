bc1da01cb8272f35d680a428c3b8cbc144aa92b9
ELEC_lead-ubcsailbot <electrical@ubcsailbot.org>
2025-03-26 12:30:17 -0700

    Create RUDDER.h
    Signed-off-by: ELEC_lead-ubcsailbot <electrical@ubcsailbot.org>

---
+++ projects/drv-modules/rudder-control-model/RUDDER.h
@@ -0,0 +1,8 @@
+/*
+ * This library implements a PID model with various extra functionality to account for dynamic sailing conditions
+ * It currently has the following functionality:
+ *		-TBD
+ *
+ *  Created on: Mar 26, 2025
+ *      Author: 
+ */
