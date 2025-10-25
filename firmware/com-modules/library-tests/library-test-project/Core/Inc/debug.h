d6ca0b3f046a73378027e7dc670bdb7740b7558d
alisha1697 <alishashahid6704@gmail.com>
2025-10-25 12:12:16 -0700

    com-modules update

---
+++ firmware/com-modules/library-tests/library-test-project/Core/Inc/debug.h
@@ -0,0 +1,22 @@
+/*
+ * 	debug.h
+ *
+ * 	Description: Provides declarations for variables and function prototypes related to debugging features.
+ *
+ *  Created on: Mar 29, 2024
+ *	Author: Peter, Jordan, Katherine
+ */
+
+#ifndef INC_OPRT_H_
+#define INC_OPRT_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "board.h"
+
+/* Variables ------------------------------------------------------------------*/
+extern uint8_t UART1_rxBuffer[1];
+
+/* Function prototypes ------------------------------------------------------------------*/
+int debug_key(void);
+
+#endif /* INC_OPRT_H_ */
