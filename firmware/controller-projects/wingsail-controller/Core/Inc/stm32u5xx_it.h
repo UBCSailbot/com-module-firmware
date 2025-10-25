d342b8072e2054b62b1f7f8aca2adc397e3c73ed
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2025-10-25 16:00:52 -0700

    Fix merge errors in stm32u5xx_it.h and update relative include paths

--- firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
+++ firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
@@ -13,7 +13,7 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
- ******************************************************************************
+  ******************************************************************************
   */
 /* USER CODE END Header */
 
@@ -25,8 +25,6 @@
  extern "C" {
 #endif
 
-<<<<<<<< HEAD:firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
-========
 /* Includes ------------------------------------------------------------------*/
 #include "stm32u5xx_hal.h"
 #include "stm32u5xx_ll_ucpd.h"
@@ -38,29 +36,7 @@
 #include "stm32u5xx_ll_pwr.h"
 #include "stm32u5xx_ll_gpio.h"
 #include "stm32u5xx_ll_dma.h"
-
 #include "stm32u5xx_ll_exti.h"
-
->>>>>>>> main:firmware/controller-projects/wingsail-controller/Core/Inc/main.h
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
-
-/* USER CODE END ET */
-
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
-
-/* USER CODE END EC */
-
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
-
-/* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
 void NMI_Handler(void);
@@ -73,45 +49,10 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void OTG_FS_IRQHandler(void);
-/* USER CODE BEGIN EFP */
 
+/* USER CODE BEGIN EFP */
 /* USER CODE END EFP */
 
-<<<<<<<< HEAD:firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
-========
-/* Private defines -----------------------------------------------------------*/
-#define USER_BUTTON_Pin GPIO_PIN_13
-#define USER_BUTTON_GPIO_Port GPIOC
-#define VBUS_SENSE_Pin GPIO_PIN_2
-#define VBUS_SENSE_GPIO_Port GPIOC
-#define UCPD_FLT_Pin GPIO_PIN_14
-#define UCPD_FLT_GPIO_Port GPIOB
-#define UCPD1_CC2_Pin GPIO_PIN_15
-#define UCPD1_CC2_GPIO_Port GPIOB
-#define LED_RED_Pin GPIO_PIN_2
-#define LED_RED_GPIO_Port GPIOG
-#define LED_GREEN_Pin GPIO_PIN_7
-#define LED_GREEN_GPIO_Port GPIOC
-#define USART1_TX_Pin GPIO_PIN_9
-#define USART1_TX_GPIO_Port GPIOA
-#define USART1_RX_Pin GPIO_PIN_10
-#define USART1_RX_GPIO_Port GPIOA
-#define USB_OTG_FS_DM_Pin GPIO_PIN_11
-#define USB_OTG_FS_DM_GPIO_Port GPIOA
-#define USB_OTG_FS_DP_Pin GPIO_PIN_12
-#define USB_OTG_FS_DP_GPIO_Port GPIOA
-#define UCPD1_CC1_Pin GPIO_PIN_15
-#define UCPD1_CC1_GPIO_Port GPIOA
-#define UCPD_DBn_Pin GPIO_PIN_5
-#define UCPD_DBn_GPIO_Port GPIOB
-#define LED_BLUE_Pin GPIO_PIN_7
-#define LED_BLUE_GPIO_Port GPIOB
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
-
->>>>>>>> main:firmware/controller-projects/wingsail-controller/Core/Inc/main.h
 #ifdef __cplusplus
 }
 #endif
