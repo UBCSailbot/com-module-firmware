f0c7a0d23373cab22cd78429d36996e11ea8ed19
Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
2025-11-29 12:07:53 -0800

    correct CAN_Receive function signature
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>

--- firmware/com-modules/CANFD/can.h
+++ firmware/com-modules/CANFD/can.h
@@ -18,7 +18,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/* CAN_frame struct for Rx buffer (standard filter). Provides Rx ID, length and buffer -*/
+/* CAN_frame struct for Rx buffer (standard filter). Provides Rx ID, length and buffer --*/
 typedef struct {
     uint32_t RxData1_Identifier;
     uint8_t  RxData1_BufferLength;
@@ -28,6 +28,6 @@ typedef struct {
 /* Function prototypes ------------------------------------------------------------------*/
 void CAN_Init(FDCAN_HandleTypeDef *hfdcan1);
 HAL_StatusTypeDef CAN_Transmit(uint32_t Identifier, uint32_t IdType, uint32_t DataLength, uint8_t* DataBuffer, FDCAN_HandleTypeDef *hfdcan1);
-HAL_StatusTypeDef CAN_Receive(uint8_t *RxData_buffer);
+HAL_StatusTypeDef CAN_Receive(CAN_Frame *frame);
 void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs);
 #endif /* SRC_CAN_H_ */
