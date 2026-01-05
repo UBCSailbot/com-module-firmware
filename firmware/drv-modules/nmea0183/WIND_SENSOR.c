de50c7948c729a1447afedaa62791fedfe51cd46
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-01-04 16:41:57 -0800

    Define interface for wind sensor

--- firmware/drv-modules/nmea0183/WIND_SENSOR.c
+++ firmware/drv-modules/nmea0183/WIND_SENSOR.c
@@ -5,39 +5,50 @@
  *      Author: george-sleen
  */
 
+#include <NMEA0183.h>
+#include <WIND_SENSOR.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <WIND_SENSOR.h>
-#include <NMEA0183.h>
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------- PFP ---------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------- OBJECT MANAGEMENT ---------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-/*
+/**
  * Creates a new WIND_SENSOR object.
  *
- * @param huartChannel Is the USART channel associated with this object. The object must not be mutated after calling this function.
+ * @param huartChannel Is the USART channel associated with this object. The
+ * object must not be mutated after calling this function.
  * @return An initialized WIND_SENSOR object.
  */
-WIND_SENSOR* WIND_SENSOR__create(NMEA0183* nmeaChannel) {
+WIND_SENSOR *WIND_SENSOR__create(NMEA0183 *nmeaChannel) {
+  if (!nmeaChannel) {
+    return NULL;
+  }
+
+  WIND_SENSOR *self = (WIND_SENSOR *)malloc(sizeof(WIND_SENSOR));
+  if (!self) {
+    return NULL;
+  }
+
+  memset(self, 0, sizeof(WIND_SENSOR));
+
+  self->channel = nmeaChannel;
+  self->reference = REFERENCE;
+  self->status = UNKNOWN;
+
+  return self;
 }
 
-/*
+/**
  * Deletes the WIND_SENSOR object.
  *
  * @param self Must be an initialized WIND_SENSOR object
  */
-void WIND_SENSOR__destroy(WIND_SENSOR* self);
+void WIND_SENSOR__destroy(WIND_SENSOR *self) {}
 
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------- HELPER FUNCTIONS ---------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------- DATA PARSING FUNCTIONS ---------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * Polls the NMEA channel and updates the WIND_SENSOR object with the most
+ * recent data.
+ *
+ * @param self must be an initialized WIND_SENSOR object (probably connected to
+ * a wind sensor if you want it to do anything useful...)
+ */
+bool WIND_SENSOR__poll(WIND_SENSOR *self) {}
--- firmware/drv-modules/nmea0183/WIND_SENSOR.h
+++ firmware/drv-modules/nmea0183/WIND_SENSOR.h
@@ -1,6 +1,9 @@
 /*
  * WIND_SENSOR.h
  *
+ * Based on the CV7-EL wind sensor:
+ * https://www.furuno.fr/docs/OPERATOR_MANUAL/LCJ-Capteurs_UserManual-CV7_ENG_18012021.pdf
+ *
  *  Created on: Nov. 15, 2025
  *      Author: george-sleen
  */
@@ -8,44 +11,73 @@
 #ifndef WIND_SENSOR_H_
 #define WIND_SENSOR_H_
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------- INCLUDES ---------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+ * Includes
+ */
 
 #include "NMEA0183.h"
 
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------- STRUCTURES ---------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+ * Type definitions
+ */
+
+typedef uint16_t tenths16_t; // Convert to fixed point by dividing by ten
+
+typedef tenths16_t wind_direction_deg_t;
+typedef tenths16_t wind_speed_knots_t;
+typedef tenths16_t wind_temp_C_t;
+
+// Based on our specific wind sensor this is what each character represents
+typedef enum {
+  VALID = 'A',
+  INVALID = 'V',
+  UNKNOWN = 0,
+} wind_status_t;
+
+// The datasheet doesn't really give enough info... This is here just to be safe
+typedef enum { REFERENCE = 'R' } wind_reference_t;
+
+/*
+ * Structures
+ */
 
 // The wind sensor object
 typedef struct {
-	float windDirectionDegrees; // One decimal point of accuracy
-	char windDirectionReference;
-	float windSpeedKnots; // One decimal point of accuracy
-	char status;
-	float windTemperatureCelcius; // One decimal point of accuracy
-	NMEA0183 nmea0183Channel;
+  wind_direction_deg_t direction;
+  wind_reference_t reference;
+  wind_speed_knots_t speed;
+  wind_status_t status;
+  wind_temp_C_t temp;
+  NMEA0183 *channel;
 } WIND_SENSOR;
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------- OBJECT MANAGEMENT ---------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 /*
+ * Management
+ */
+
+/**
  * Creates a new WIND_SENSOR object.
  *
- * @param huartChannel Is the USART channel associated with this object. The object must not be mutated after calling this function.
+ * @param huartChannel Is the USART channel associated with this object. The
+ * object must not be mutated after calling this function.
  * @return An initialized WIND_SENSOR object.
  */
-WIND_SENSOR* WIND_SENSOR__create(NMEA0183* nmeaChannel);
+WIND_SENSOR *WIND_SENSOR__create(NMEA0183 *nmeaChannel);
 
-/*
+/**
  * Deletes the WIND_SENSOR object.
  *
  * @param self Must be an initialized WIND_SENSOR object
  */
-void WIND_SENSOR__destroy(WIND_SENSOR* self);
+void WIND_SENSOR__destroy(WIND_SENSOR *self);
 
+/**
+ * Polls the NMEA channel and updates the WIND_SENSOR object with the most
+ * recent data.
+ *
+ * @param self must be an initialized WIND_SENSOR object (probably connected to
+ * a wind sensor if you want it to do anything useful...)
+ */
+bool WIND_SENSOR__poll(WIND_SENSOR *self);
 
 #endif /* WIND_SENSOR_H_ */
