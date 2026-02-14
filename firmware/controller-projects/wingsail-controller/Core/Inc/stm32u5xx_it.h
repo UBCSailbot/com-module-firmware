2f4a064cf2afeb2de4b9e12fca9cc4923980199b
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-02-14 12:13:06 -0800

    Tentative fix

--- firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
+++ firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
@@ -55,6 +55,7 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void FDCAN1_IT0_IRQHandler(void);
 void USART2_IRQHandler(void);
 void UART5_IRQHandler(void);
 void OTG_FS_IRQHandler(void);
--- firmware/controller-projects/wingsail-controller/Core/Src/stm32u5xx_it.c
+++ firmware/controller-projects/wingsail-controller/Core/Src/stm32u5xx_it.c
@@ -56,6 +56,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern FDCAN_HandleTypeDef hfdcan1;
 extern DMA_HandleTypeDef handle_GPDMA1_Channel14;
 extern DMA_HandleTypeDef handle_GPDMA1_Channel15;
 extern UART_HandleTypeDef huart5;
@@ -202,6 +203,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32u5xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles FDCAN1 interrupt 0.
+  */
+void FDCAN1_IT0_IRQHandler(void)
+{
+  /* USER CODE BEGIN FDCAN1_IT0_IRQn 0 */
+
+  /* USER CODE END FDCAN1_IT0_IRQn 0 */
+  HAL_FDCAN_IRQHandler(&hfdcan1);
+  /* USER CODE BEGIN FDCAN1_IT0_IRQn 1 */
+
+  /* USER CODE END FDCAN1_IT0_IRQn 1 */
+}
 
 /**
   * @brief This function handles USART2 global interrupt.
