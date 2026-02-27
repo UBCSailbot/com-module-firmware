c25b91091c4e6c7ba3b39e155b55c38dae5948f6
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-02-27 12:37:16 -0800

    Update to include CAN heartbeat

--- firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
+++ firmware/controller-projects/wingsail-controller/Core/Inc/stm32u5xx_it.h
@@ -56,6 +56,7 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void FDCAN1_IT0_IRQHandler(void);
+void TIM7_IRQHandler(void);
 void USART2_IRQHandler(void);
 void UART5_IRQHandler(void);
 void OTG_FS_IRQHandler(void);
--- firmware/controller-projects/wingsail-controller/Core/Src/main.c
+++ firmware/controller-projects/wingsail-controller/Core/Src/main.c
@@ -64,6 +64,8 @@ DMA_HandleTypeDef handle_GPDMA1_Channel15;
 
 SPI_HandleTypeDef hspi1;
 
+TIM_HandleTypeDef htim7;
+
 PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
 /* USER CODE BEGIN PV */
@@ -95,6 +97,7 @@ static void MX_FDCAN1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_UART5_Init(void);
 static void MX_LPUART1_UART_Init(void);
+static void MX_TIM7_Init(void);
 /* USER CODE BEGIN PFP */
 #ifdef __GNUC__
 /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
@@ -152,9 +155,10 @@ int main(void)
   MX_SPI1_Init();
   MX_UART5_Init();
   MX_LPUART1_UART_Init();
+  MX_TIM7_Init();
   /* USER CODE BEGIN 2 */
   CANSPI_Initialize();
-  CAN_Init(&hfdcan1);
+  CAN_Init(&hfdcan1, 0x132);
 
   // Power cycle all attached sensors
   HAL_GPIO_WritePin(LIGHT_GATE_GPIO_Port, LIGHT_GATE_Pin, GPIO_PIN_RESET);
@@ -704,6 +708,44 @@ static void MX_SPI1_Init(void)
   /* USER CODE BEGIN SPI1_Init 2 */
 
   /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief TIM7 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM7_Init(void)
+{
+
+  /* USER CODE BEGIN TIM7_Init 0 */
+
+  /* USER CODE END TIM7_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM7_Init 1 */
+
+  /* USER CODE END TIM7_Init 1 */
+  htim7.Instance = TIM7;
+  htim7.Init.Prescaler = 31999;
+  htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim7.Init.Period = 49999;
+  htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim7, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM7_Init 2 */
+
+  /* USER CODE END TIM7_Init 2 */
 
 }
 
