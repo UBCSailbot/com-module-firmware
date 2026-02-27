dda8a6a5288f008e4854875d68749b802b914f6f
George Sleen <147893275+georgesleen@users.noreply.github.com>
2026-02-27 17:19:01 -0500

    Wingsail CAN Heartbeat Update (#44)
    * can.h first commit
    
    Should work? unless I overlooked something or tested it wrong
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    
    * can.c first commit
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    
    * Documentation of can.c
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    
    * Documented can.h
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    
    * Documentation complete can.c
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    
    * fixed frame range
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    
    * filter works...too much
    
    - receives out of range IDs
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * CANFD project file
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * Update can.c
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * Update can.h
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * fixed parsing in test_canfd.c
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * Updated can.c with CAN_Receive and fixed definition error
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * Updated can.h
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * FDCAN_Serial_Testing Zip CUBEIDE project file
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * com-modules update
    
    * removed files
    
    * Moved can files (#37): accepted CAN files move to CANFD dir, removed from base-library dir
    
    * updated README on flle usage + cleanup
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * changed CAN_Receive to use a CAN_Frame rx pointer
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * correct CAN_Receive function signature
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * fdcan-library-test now has the project to test
    
    * fdcan-library-test is now a project
    
    * Add heartbeat functionality to CANFD module
    
    Updated CAN_Init to include heartbeat ID and added heartbeat functionality with TIM7.
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * Modify CAN_Init to accept hardware ID parameter
    
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    
    * Updated fdcan-library-test with overnight test code, includes heartbeat
    
    * Update to include CAN heartbeat
    
    ---------
    
    Signed-off-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    Signed-off-by: Alisha Siddiqui <143849351+alisha1697@users.noreply.github.com>
    Co-authored-by: alisha1697 <143849351+alisha1697@users.noreply.github.com>
    Co-authored-by: alisha1697 <alishashahid6704@gmail.com>
    Co-authored-by: Michael Greenough <43833591+MichaelGreenough@users.noreply.github.com>

--- firmware/com-modules/CANFD/can.h
+++ firmware/com-modules/CANFD/can.h
@@ -26,7 +26,7 @@ typedef struct {
 } CAN_Frame;
 
 /* Function prototypes ------------------------------------------------------------------*/
-void CAN_Init(FDCAN_HandleTypeDef *hfdcan1);
+void CAN_Init(FDCAN_HandleTypeDef *hfdcan1, uint32_t hbid);
 HAL_StatusTypeDef CAN_Transmit(uint32_t Identifier, uint32_t IdType, uint32_t DataLength, uint8_t* DataBuffer, FDCAN_HandleTypeDef *hfdcan1);
 HAL_StatusTypeDef CAN_Receive(CAN_Frame *frame);
 void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs);
--- firmware/controller-projects/wingsail-controller/Core/Src/stm32u5xx_it.c
+++ firmware/controller-projects/wingsail-controller/Core/Src/stm32u5xx_it.c
@@ -61,6 +61,7 @@ extern DMA_HandleTypeDef handle_GPDMA1_Channel14;
 extern DMA_HandleTypeDef handle_GPDMA1_Channel15;
 extern UART_HandleTypeDef huart5;
 extern UART_HandleTypeDef huart2;
+extern TIM_HandleTypeDef htim7;
 extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 /* USER CODE BEGIN EV */
 
@@ -216,6 +217,20 @@ void FDCAN1_IT0_IRQHandler(void)
   /* USER CODE BEGIN FDCAN1_IT0_IRQn 1 */
 
   /* USER CODE END FDCAN1_IT0_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM7 global interrupt.
+  */
+void TIM7_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM7_IRQn 0 */
+
+  /* USER CODE END TIM7_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim7);
+  /* USER CODE BEGIN TIM7_IRQn 1 */
+
+  /* USER CODE END TIM7_IRQn 1 */
 }
 
 /**
