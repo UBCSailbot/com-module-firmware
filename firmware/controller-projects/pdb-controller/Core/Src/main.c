2c2227b7350f7e429fea1d7311bcbaf6ac0b50f0
Michael Greenough <michaellg2004@gmail.com>
2026-08-10 15:53:11 -0700

    Added reset line in error handler

--- firmware/controller-projects/pdb-controller/Core/Src/main.c
+++ firmware/controller-projects/pdb-controller/Core/Src/main.c
@@ -1319,6 +1319,7 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
+  NVIC_SystemReset();
   while (1)
   {
   }
